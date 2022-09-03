7dab28531ff6efe74d37e85896628087b7a55541
paulinho255 <paulinho255@hotmail.com>
2022-09-03 12:04:15 -0300

    Primeiro commit do projeto react para manipulação de estado de componentes.

---
+++ src/components/Products/Product.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Product.css'
+
+const Product = () => {
+    (
+        <div className='wrapper'>
+            <div>
+                Shopping Cart: 0 total items.
+            </div>
+            <div>Total: 0</div>
+
+            <div className='product'>
+                <span role='img' aria-label='ice cream'>🍦</span>
+            </div>
+            <button>Add</button> <button>Remove</button>
+        </div>
+    )
+}
+
+export default Product
